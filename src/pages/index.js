7fb13afb9fa71b2b09eb3457c4b3f7464bcd700d
Igor <igor06brw@gmail.com>
2020-11-14 18:10:26 +0100

    clean index.js workspace

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,19 +7,9 @@ import SEO from "../components/seo"
 import Nav from "../components/nav"
 
 const IndexPage = () => (
-  <Layout>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+  <>
     <Nav></Nav>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
