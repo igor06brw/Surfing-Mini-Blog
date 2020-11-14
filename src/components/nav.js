53b79f3f7a835a8c866b13a57768285a145ece85
Igor <igor06brw@gmail.com>
2020-11-14 18:01:10 +0100

    start writing social media icons list

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,32 @@
 import React from "react"
+import styled from "styled-components"
 
-const Nav = () => (
+const StyledNav = styled.nav`
+    max-width: 1725px;
+`
+const StyledUl = styled.ul`
     
-)
+`
+
+const StyledLi = styled.li`
+
+` 
+const StyledLink = styled.a`
+
+`
+
+
+const Nav = () => {
+
+    return (
+        <StyledNav>
+            <StyledUl>  
+                <StyledLi>
+                    <StyledLink></StyledLink>
+                </StyledLi>
+            </StyledUl>
+        </StyledNav>
+    )
+}
 
 export default Nav
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Nav from "../components/nav"
 
 const IndexPage = () => (
   <Layout>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -14,6 +16,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Nav></Nav>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
