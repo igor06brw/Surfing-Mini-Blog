97775c3b250d4cb9002535a4723435f9be667845
Igor <igor06brw@gmail.com>
2020-11-14 21:56:14 +0100

    install gatsby-background-image, setup query, config bgImage

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,50 @@
 import React from "react"
 
 import Nav from "../components/nav"
+import styled from "styled-components"
+import { graphql, useStaticQuery } from 'gatsby'
+import BackgroundImage from "gatsby-background-image"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <Nav></Nav>
-  </header>
-)
+const StyledContainer = styled.div`
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+`
+
+const StyledBackground = styled(BackgroundImage)`
+  display: block;
+  width: 100vw;
+  height: 80vh;
+  position: absolute;
+  overflow: hidden;
+`
+
+const Header = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        file(relativePath: {eq: "bg.jpg"}) {
+          childImageSharp {
+            fluid(quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `
+  )
+  const imageData = data.file.childImageSharp.fluid
+
+  
+  return (
+    <header>
+      <Nav></Nav>
+      <StyledContainer>
+        <StyledBackground Tag="div" fluid={imageData}>
+        </StyledBackground>
+      </StyledContainer>
+    </header>
+  )
+}
 
 export default Header
