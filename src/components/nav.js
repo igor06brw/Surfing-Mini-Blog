0afd3ae5fdb7e6edb841e81f30e8ff1aa539dba4
Igor <igor06brw@gmail.com>
2020-11-14 20:13:11 +0100

    finish justify of nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,30 +3,45 @@ import styled from "styled-components"
 import logo from "../images/logo.png"
 
 const StyledNav = styled.nav`
+    padding: 10px 50px;
     max-width: 1725px;
     display: flex;
     flex-direction: row;
     align-items: center;
+    justify-content: space-between
 `
 const StyledUl = styled.ul`
 `
 
 const StyledLi = styled.li`
     display: inline;
+    padding: 0 1.5em;
+
+
 ` 
 const StyledLink = styled.a`
-
+    width: 100%;
 `
 
 const Wrapper = styled.div`
     display: flex;
+    height: 100%;
+    width: 33%;
+
+    &:nth-of-type(2) {
+        justify-content: center;
+    }
+
+    &:nth-of-type(3) {
+        justify-content: flex-end;
+    }
 `
 
 const Hamburger = styled.a`
     margin: 20px;
     display: block;
     position: relative;
-    height: 20px;
+    height: 25px;
     width: 30px;
     cursor: pointer;
 `
