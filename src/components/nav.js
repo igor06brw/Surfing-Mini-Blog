f29a46da01843a674f79cbc18da0ea432d8b6b39
Igor <igor06brw@gmail.com>
2020-11-14 18:20:16 +0100

    add hamburger menu & logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import logo from "../images/logo.png"
 
 const StyledNav = styled.nav`
     max-width: 1725px;
@@ -15,25 +16,52 @@ const StyledLink = styled.a`
 
 `
 
+const Wrapper = styled.div`
+
+`
+
+const Hamburger = styled.a`
+
+`
+
+const Line = styled.span`
+    background-color: #cdcdcd;
+`
+
+
+
 
 const Nav = () => {
 
     return (
         <StyledNav>
-            <StyledUl>  
-                <StyledLi>
-                    <StyledLink><i class="fab fa-facebook-f"></i></StyledLink>
-                </StyledLi>
-                <StyledLi>
-                    <StyledLink><i class="fab fa-twitter"></i></StyledLink>
-                </StyledLi>
-                <StyledLi>
-                    <StyledLink><i class="fab fa-pinterest"></i></StyledLink>
-                </StyledLi>
-                <StyledLi>
-                    <StyledLink><i class="fab fa-instagram"></i></StyledLink>
-                </StyledLi>
-            </StyledUl>
+
+            <Wrapper>
+                <Hamburger>
+                    <Line />
+                    <Line />
+                    <Line />
+                </Hamburger>
+            </Wrapper>
+            <Wrapper>
+                <img src={logo} />
+            </Wrapper>
+            <Wrapper>
+                <StyledUl>  
+                    <StyledLi>
+                        <StyledLink><i class="fab fa-facebook-f"></i></StyledLink>
+                    </StyledLi>
+                    <StyledLi>
+                        <StyledLink><i class="fab fa-twitter"></i></StyledLink>
+                    </StyledLi>
+                    <StyledLi>
+                        <StyledLink><i class="fab fa-pinterest"></i></StyledLink>
+                    </StyledLi>
+                    <StyledLi>
+                        <StyledLink><i class="fab fa-instagram"></i></StyledLink>
+                    </StyledLi>
+                </StyledUl>
+            </Wrapper>
         </StyledNav>
     )
 }
