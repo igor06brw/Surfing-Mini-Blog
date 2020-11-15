49c11ab853d5bad3547b95638e4cf0b03569145b
Igor <igor06brw@gmail.com>
2020-11-15 03:41:55 +0100

    add bg-2 as query to backgroundImage

--- src/components/section-two.js
+++ src/components/section-two.js
@@ -1,5 +1,7 @@
 import React from "react"
 import styled from "styled-components"
+import { graphql, useStaticQuery } from 'gatsby'
+import BackgroundImage from "gatsby-background-image"
 
 const Section = styled.section`
     max-width:1725px;
@@ -8,16 +10,44 @@ const Section = styled.section`
 const Container = styled.div`
     margin: 0 auto;
     background-color: white;
-    height: 200px;
+    height: 500px;
     max-width: 1140px;
+    overflow-x: hidden;
+`
+
+const CustomBackground = styled(BackgroundImage)`
+    width: 100%;
+`
+
+const Wrapper = styled.div`
 `
 
 
 const SectionTwo= () => {
+    const data = useStaticQuery(
+        graphql`
+          query {
+            file(relativePath: {eq: "bg-2.jpg"}) {
+              childImageSharp {
+                fluid(quality: 100, maxWidth: 3000) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        `
+    )
+    const imageData = data.file.childImageSharp.fluid
+
     return (
         <Section>
             <Container>
-            
+                <CustomBackground Tag="div" fluid={imageData}>
+                    <Wrapper>
+                        <h3>When you get a good group of babes together, there’s nothing better!" </h3>
+                        <p>An elegant, modern and fully cutomizable UI Kit</p>
+                    </Wrapper>
+                </CustomBackground>
             </Container>
         </Section>
     )
