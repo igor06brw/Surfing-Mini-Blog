8f5055c7be5960b08c9d6edeb3a8954a13ed4d27
Igor <igor06brw@gmail.com>
2020-11-16 20:24:55 +0100

    finish content of Article for DatoCMS

--- src/templates/article-post.js
+++ src/templates/article-post.js
@@ -1,11 +1,47 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
 
-const Article = (props) => {
+const Article = styled.div`
+
+`
+
+const ArticleImage = styled.img`
+
+`
+
+const ArticleContent = styled.div`
+
+`
+
+const ArticleDate = styled.p`
+
+`
+
+const ArticleHeader = styled.h3`
+
+`
+
+const ArticleText = styled.p`
+
+`
+
+const ArticleLink = styled(Link)`
+
+`
+
+const ArticlePost = (props) => {
     return (
-        <>
-        </>
+        <Article>
+            <ArticleImage />
+            <ArticleContent>
+                <ArticleDate>Friday, October 7,  2017</ArticleDate>
+                <ArticleHeader>NLand Surf Park Opening</ArticleHeader>
+                <ArticleText>is is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor. This is      Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor. This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</ArticleText>
+                <ArticleLink>Read More</ArticleLink>
+            </ArticleContent>
+        </Article>
     );
 }
 
-export default Article;
+export default ArticlePost;
